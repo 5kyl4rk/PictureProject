67a60fc6fefaffb642672cf944cddef7ed5e1de5
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2019-05-13 11:54:21 -0600

    Frame will now terminate the program on close

--- src/pix/view/GlitchFrame.java
+++ src/pix/view/GlitchFrame.java
@@ -22,9 +22,9 @@ public class GlitchFrame extends JFrame
 		this.setContentPane(appPanel);
 		this.setMinimumSize(appPanel.getToolPanelSize());
 		this.setLocationRelativeTo(null);
-
 		this.setResizable(true);
 		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 	public void updateDisplay()
@@ -43,6 +43,8 @@ public class GlitchFrame extends JFrame
 		
 		appPanel.updateDisplay();
 	}
+	
+	
 	
 	public void recenter()
 	{
