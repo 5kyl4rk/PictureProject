eb74df77886dd4be378c35676e0361fb61ce074d
5kyl4rk <37351525+5kyl4rk@users.noreply.github.com>
2019-09-03 12:19:27 -0600

    Create BetterImageStack.java

---
+++ src/pix/model/BetterImageStack.java
@@ -0,0 +1,14 @@
+package pix.model;
+
+import java.util.ArrayList;
+
+import pix.controller.PixController;
+
+public class BetterImageStack
+{
+	private ArrayList<EditProfile> editList;
+	public BetterImageStack(PixController app)
+	{
+		
+	}
+}
